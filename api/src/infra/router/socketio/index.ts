4e81bda9593c9cf5929cba39be7beafc409b1082
joao victor <joao99sb@gmail.com>
2024-09-01 22:34:46 -0300

    fix: cors

--- api/src/infra/router/socketio/index.ts
+++ api/src/infra/router/socketio/index.ts
@@ -21,7 +21,6 @@ export class MountSocketHandler {
     this.io = new Server(this.server, {
       cors: {
         origin: "*",
-        methods: ["GET", "POST"]
       }
     })
     this.streamController = streamController
